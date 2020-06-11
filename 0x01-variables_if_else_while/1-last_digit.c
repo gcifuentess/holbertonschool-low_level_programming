6f92fb07a91394f42d180f6ced402d06594012f1
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-11 14:23:37 -0500

    typo fixed: grater > greater

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	ld = n - (n / 10) * 10;
 	if (ld > 5)
-		printf("Last digit of %d is %d and is grater than 5\n", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else if (ld == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	else
