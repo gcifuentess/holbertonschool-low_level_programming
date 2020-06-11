5b42b3732cce891e5ecc55f4e8317b1723b52746
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-11 17:20:16 -0500

    File created

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: always 0
+ */
+
+int main()
+{
+	int i;
+	char l[50];
+
+	for (i = 1; i <= 50; i++)
+	{
+		l[i] = i + 96;
+		if (l[i] <= 122)
+		{
+			putchar(l[i]);
+		}
+		else
+		{
+			l[i] = i + 38;
+			putchar(l[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
