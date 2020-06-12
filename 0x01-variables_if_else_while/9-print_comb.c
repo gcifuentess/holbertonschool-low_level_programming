392680d52b39d20b73e8def0557c077d6d41853b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-11 20:08:47 -0500

    File created

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n != 9)
+		{
+			putchar('0' + n);
+			putchar(',');
+		}
+		else
+		{
+			putchar('0' + n);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
