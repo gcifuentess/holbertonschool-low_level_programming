e448adf3963452e341976276176b035e773648b2
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-11 19:48:06 -0500

    File created

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+	for (n = 97; n < 103; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
