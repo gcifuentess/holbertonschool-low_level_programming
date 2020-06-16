f1b54f6c1179ae101c7708ce1b2e26b67bf78e77
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-16 01:01:15 -0500

    File created

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, k, toprint;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 1; k <= 2; k++)
+			{
+				toprint = i * j;
+				if (k == 1)
+				{
+					if (toprint >= 10)
+						toprint = toprint / 10;
+					else
+						toprint = -16;
+					_putchar('0' + toprint);
+				}
+				else
+				{
+					toprint = toprint % 10;
+					_putchar('0' + toprint);
+					break;
+				}
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
