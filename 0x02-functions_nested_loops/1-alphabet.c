148b755473d03f27712785090e72233bf67d089d
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-15 22:17:10 -0500

    File created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints de alphabet, in lower case followed by new line
+ *
+ * Return: Always void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
