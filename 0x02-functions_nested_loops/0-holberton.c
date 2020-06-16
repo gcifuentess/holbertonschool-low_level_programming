4eaccff0c36095dd319b2320605ede7052736f49
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-15 22:03:15 -0500

    Code fixed - The whole code was fixed in order to use only 1 _putchar function

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,49 @@
 #include "holberton.h"
-
-/** main - Prints Holberton
+/**
+ * main - Prints Holberton
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	int i, j, word;
+
+	i = 1;
+	while (i <= 9)
+	{
+		j = 72;
+		while (j <= 122)
+		{
+			if ((i == 1 && j == 72) || (i == 2 && j == 111))
+			{
+				word = j;
+				i++;
+				j = 123;
+			}
+			else if (i == 3 && j == 108)
+			{
+				word = j;
+				i++;
+				j = 123;
+			}
+			else if ((i == 4 && j == 98) || (i == 5 && j == 101)
+				 || (i == 6 && j == 114)
+				 || (i == 7 && j == 116))
+			{
+				word = j;
+				i++;
+				j = 123;
+			}
+			else if ((i == 8 && j == 111) || (i == 9 && j == 110))
+			{
+				word = j;
+				i++;
+				j = 123;
+			}
+			j++;
+		}
+		_putchar(word);
+	}
 	_putchar('\n');
 	return (0);
 }
