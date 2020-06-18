7fc1626596c89ca0f45066b359eecec809540765
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-18 14:49:28 -0500

    File created

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,12 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 14; i++)
-			_putchar('0' + i);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9 && i <= 14)
+				_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+		}
 		_putchar('\n');
 	}
 }
