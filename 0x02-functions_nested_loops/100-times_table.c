fd368e1ac893340e9028d4ce7f26ef31e5d4871b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-17 23:52:28 -0500

    File created

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,45 @@
 #include "holberton.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0
- * If n is greater than 15 or less than 0 the function
- * should not print anything
  * @n: int value for  n times
  * Return: Void
  */
 void print_times_table(int n)
 {
-	int i, j, toprint;
+	int i, j;
 
-	if (n <= 15 && n >= 0)
+	for (i = 0; i <= n && n >= 0 && n <= 15; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			if (i * j >= 100)
 			{
-				toprint = i * j;
-				if (toprint >= 100)
-				{
-					_putchar('0' + toprint / 100);
-					_putchar('0' + toprint / 10
-						 - toprint / 100 * 10);
-					_putchar('0' + toprint % 10);
-				}
-				else if (toprint >= 10)
+				_putchar('0' + i * j / 100);
+				_putchar('0' + i * j / 10
+					 -  i * j / 100 * 10);
+				_putchar('0' + i * j % 10);
+			}
+			else if (i * j >= 10)
+			{
+				_putchar('0' - 16);
+				_putchar('0' + i * j / 10);
+				_putchar('0' + i * j % 10);
+			}
+			else
+			{
+				if (j > 0)
 				{
 					_putchar('0' - 16);
-					_putchar('0' + toprint / 10);
-					_putchar('0' + toprint % 10);
+					_putchar('0' - 16);
 				}
-				else
-				{
-					if (j > 0)
-					{
-						_putchar('0' - 16);
-						_putchar('0' - 16);
-					}
-					_putchar('0' + toprint % 10);
-				}
-				/*-16 to have the space char*/
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar('0' + i * j % 10);
+			} /*-16 to have the space char*/
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
