4ff7838c10a4d186d42e7fc8a2fcba777f724f3e
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-18 20:38:21 -0500

    File created

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: int value to print
+ * Return: Void
+ */
+void print_number(int n)
+{
+	int i, j, module;
+
+	j = 0;
+	module = 10;
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+	}
+	for (i = n; i >= 10; i = i / 10)
+	{
+		module *= 10;
+		j++;
+	}
+
+	for (j = j; j >= 0; j--)
+	{
+		_putchar((n % (module) / ((module) / 10)) + '0');
+		module /= 10;
+	}
+}
