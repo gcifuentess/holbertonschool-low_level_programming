112432644b062b49fdace2b0f7e7befd4a31672e
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-18 23:28:15 -0500

    File created

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int fibo1 = 1, fibo2 = 2, fibo3;
+
+	printf("%lu, ", fibo1);
+	printf("%lu, ", fibo2);
+
+	for (i = 1; i < 97; i++)
+	{
+		fibo3 = fibo2;
+		fibo2 = fibo1 + fibo2;
+		fibo1 = fibo3;
+		printf("%lu", fibo2);
+		if (i < 96)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
