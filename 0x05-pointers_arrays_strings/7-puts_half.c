3c70799cd7d8140b99812deb7749c02323726a18
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-23 15:21:08 -0500

    File created

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of a string,
+ *
+ * Specs: The function should print the second half of the string
+ * If the number of characters is odd, the function should
+ * print the last n characters of the string,
+ * where n = (length_of_the_string - 1) / 2
+ * @str: The string to compute
+ *
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+	int i, len, count;
+
+	count = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	count --;
+
+	len = count;
+	if (len % 2 != 0)
+		len = len / 2;
+	else
+		len = (len - 1) / 2;
+
+	for (i = count - len; i <= count; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
