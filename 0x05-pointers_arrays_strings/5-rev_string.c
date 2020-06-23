fa9d6f7a045df40bb2e635b9ab266e380b21781f
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-23 18:09:36 -0500

    The temp array was changed for a single variable

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,14 @@
  */
 void rev_string(char *s)
 {
-	int i, count = 0;
-	char temp[256];
+	int i, temp, count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		temp[i] = s[i];
 		count++;
-	}
 	count--;
 	for (i = count; i >= 0; i--)
-		s[count - i] = temp[i];
+	{
+		temp = s[i];
+		s[count - i] = temp;
+	}
 }
