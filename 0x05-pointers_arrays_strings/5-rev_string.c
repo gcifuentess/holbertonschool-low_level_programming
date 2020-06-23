0d836f8028a7416436b9653f56fb2642de53a430
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-23 14:35:21 -0500

    File created

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string
+ * @s: The string to reverse
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	int i, count = 0;
+	char temp[101];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		temp[i] = s[i];
+		count++;
+	}
+	count--;
+	for (i = count; i >= 0; i--)
+		s[count - i] = temp[i];
+}
