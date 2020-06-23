88edf37ce888732c30f8bc639a00a7cdc9c6508c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-23 13:46:39 -0500

    File created

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s: The string to print
+ *
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
