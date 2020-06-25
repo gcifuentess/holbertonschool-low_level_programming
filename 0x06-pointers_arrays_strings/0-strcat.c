00472517ef29946c01d38e59dc47ac4b5817ebb9
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-25 17:21:54 -0500

    Betty Style fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,17 +2,16 @@
 
 /**
  * _strcat - concatenates two strings
- *
+ * @dest: is the destination pointer
+ * @src: is the source pointer
  * Return: a pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++) /*just increment i*/
-	{
-	}
-
+	for (i = 0; dest[i] != '\0';) /*just increment i*/
+		i++;
 	for (j = 0; src[j] != '\0'; j++)
 		dest[i + j] = src[j];
 
