fbac8ed19513a2f57091e7b58bbbcbc7589cfa07
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-25 23:00:53 -0500

    Big logic changed, moved from if statemts to use of for and arrays

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,17 +8,28 @@
  */
 char *rot13(char *s)
 {
-	/*int letter[50] = {a, A, b, B, c, C, d, D, e, E, f, F, g, G, h, H }*/
-	int i;
+	int letter[52] = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f',
+			  'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K',
+			  'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q',
+			  'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V',
+			  'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'};
+	int code[52] = {'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's',
+			'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X',
+			'y', 'Y', 'z', 'Z', 'a', 'A', 'b', 'B', 'c', 'C', 'd',
+			'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I',
+			'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M'};
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
-			s[i] = s[i] + 13;
-		else if (s[i] >= 'n' && s[i] <= 'z')/*Start over range 'a'-'z'*/
-			s[i] = 13 - ('z' - s[i]) - 1 + 'a';
-		else if (s[i] >= 'N' && s[i] <= 'Z')/*Start over range 'A'-'Z'*/
-			s[i] = 13 - ('Z' - s[i]) - 1 + 'A';
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = code[j];
+				break;
+			}
+		}
 	}
 
 	return (s);
