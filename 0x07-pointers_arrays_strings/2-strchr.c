a44db1f0af76d8d36c92da4038bbc8bd255013ca
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-28 09:00:22 -0500

    File created

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: The pointer to the space of memory to analize
+ * @c: the character to locate
+ *
+ * Return: the pointer of the located @c
+ */
+char *_strchr(char *s, char c)
+{
+	char *located;
+	int i;
+
+	located = NULL;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			located = s + i;
+			break;
+		}
+	}
+
+	return (located);
+}
