e666a844f4ff44d4eed9c6157c960e121d1e8821
Gabriel Cifuentes <1795@holbertonschool.com>
2020-06-29 18:22:30 -0500

    Reversed to the previous logic but without creating a new pointer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != c && s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
 	}
-	if (s[i] == c)
-	{
-		s = &s[i];
-		return (s);
-	}
-	else
-		return (0);
+
+	return (0);
 }
