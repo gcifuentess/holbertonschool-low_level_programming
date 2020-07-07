9720210edfc1b9095a039785c610c03e315f0e98
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-07 17:48:23 -0500

    File created

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *    which contains a copy of the string given as a parameter.
+ * @str: the string to be copied.
+ *
+ * Description: -Returns a pointer to a new string which is a duplicate
+ *               of the string str. Memory for the new string is obtained
+ *               with malloc, and can be freed with free.
+ *              -Returns NULL if str = NULL.
+ *
+ * Return: On success, the _strdup function returns a pointer to the
+ *         duplicated string. It returns NULL if insufficient memory
+ *         was available
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	ptr = malloc(sizeof(char) * i);
+	for (j = 0; j < i; j++)
+	{
+		ptr[j] = str[j];
+	}
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	return (ptr);
+}
+
