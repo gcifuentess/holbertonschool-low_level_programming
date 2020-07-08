9402ecb76a6cc070697ad7822796ca48de642df3
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-07 22:59:57 -0500

    Template

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * strtow - desc
+ * @str: desc
+ *
+ * Return: desc
+ */
+char **strtow(char *str)
+{
+	char **dprt;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	dptr = malloc(1);
+
+	if (dptr == NULL)
+		return (NULL);
+
+	return dptr;
+}
