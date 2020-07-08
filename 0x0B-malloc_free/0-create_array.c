f057216ad18009d18c3fdebacc40a8d4b18d81f4
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-08 00:27:35 -0500

    Order to check malloc's return changed - moved lines up

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,14 +20,11 @@ char *create_array(unsigned int size, char c)
 
 	ptr = malloc(sizeof(char) * size);
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
-
-	if (ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 
 	return (ptr);
 }
