931d9b21d4dec527a6ca8ef70b52ee7814d4e3c7
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-09 15:08:05 -0500

    The argument of the name of the function was not included and is requested - fixed

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -19,7 +19,7 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			flen += 1;
@@ -31,14 +31,13 @@ char *argstostr(int ac, char **av)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 1, k = 0; i < ac; i++)
+	for (i = 0, k = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			ptr[k++] = av[i][j];
 		ptr[k++] = '\n';/*replacing '\0' with '\n'*/
 	}
 	ptr[k] = '\0';
-	printf("%s\n", ptr);
 
 	return (ptr);
 }
