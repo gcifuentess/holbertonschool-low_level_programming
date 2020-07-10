88bf43bfa9f47ab232ec6318137a2c51cc7d244a
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-09 22:30:33 -0500

    Template

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * string_nconcat - desc
+ * @s1: desc
+ * @s2: desc
+ * @n: des
+ *
+ * Return: des
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+
+	if (!s1)
+		*s1 = "";
+	if (!s2)
+		*s2 = "";
+
+	ptr = malloc(1);
+
+	if (!ptr)
+		return (NULL);
+
+	return (ptr);
+}
