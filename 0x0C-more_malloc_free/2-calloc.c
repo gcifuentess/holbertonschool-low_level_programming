9b66fd7e073fb9ff687b0d78e8ff76612dfcd148
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-09 22:46:29 -0500

    Template

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
 
 	if (!ptr)
 		return (NULL);
