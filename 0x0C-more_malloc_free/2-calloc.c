a9e369f76f090d98de461b81800ae3597c16ce59
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-11 13:27:41 -0500

    File created

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,24 @@
 #include "holberton.h"
 
 /**
- * _calloc - desc
- * @nmemb: des
- * @size: des
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements of the array.
+ * @size: size in bytes of each element of the array.
  *
- * Return: des
+ * Description: -The _calloc function allocates memory for an array
+ *               of nmemb elements of size bytes each and returns a
+ *		 pointer to the allocated memory.
+ *		-The memory is set to zero.
+ *		-If nmemb or size is 0, then _calloc returns NULL.
+ *		-If malloc fails, then _calloc returns NULL
+ *
+ * Return: pointer to the allocated memory.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	int *tmp_ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -18,6 +27,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!ptr)
 		return (NULL);
+
+	tmp_ptr = (int *)ptr;
+
+	for (i = 0; i < (nmemb / 4); i++)
+		tmp_ptr[i] = 0;
 
 	return (ptr);
 }
