878c6010dc3bcd78c97020b633bfa17fbc59bf01
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-10 19:56:27 -0500

    File created

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,41 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	(void)n;
+	unsigned int len1 = 0, len2 = 0, concat, temp;
 
 	if (!s1)
-		s1 = "";
+		len1 = 1;
 	if (!s2)
-		s2 = "";
+		len2 = 1;
 
-	ptr = malloc(1);
+	if (len1 == 0)
+	{
+		for (; s1[len1] != '\0'; len1++)
+		{
+		}
+	}
+	if (len2 == 0)
+	{
+		for (; s2[len2] != '\0'; len2++)
+		{
+		}
+		len2++;
+	}
+	if (len2 > n)
+		len2 = n;
 
+	ptr = malloc(sizeof(char) * (len1 + len2));
 	if (!ptr)
 		return (NULL);
+
+	for (concat = 0, temp = 0; concat < (len1 + len2); concat++)
+	{
+		if (concat < len1)
+			ptr[concat] = s1[concat];
+		else
+			ptr[concat] = s2[temp++];
+	}
+	ptr[concat] = '\0';
 
 	return (ptr);
 }
