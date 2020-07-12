6ccdfe1e1b3f20808806f01459a3a0f736c16df8
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-11 21:08:20 -0500

    File created

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- * @argc: des
- * @argv: des
+ * main - multiplies two positive numbers.
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
-	unsigned long int mul, num1, num2;
+	unsigned long int mul, num1, num2, i;
 
 	if (argc > 3)
 	{
@@ -17,7 +17,24 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
+	for (i = 0; argv[1][i] != '\0'; i++)
+	{
+		if (atoi(&argv[1][i]) == 0 && argv[1][i] != '0')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 	num1 = atoi(argv[1]);
+
+	for (i = 0; argv[2][i] != '\0'; i++)
+	{
+		if (atoi(&argv[2][i]) == 0 && argv[2][i] != '0')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 	num2 = atoi(argv[2]);
 
 	mul = num1 * num2;
