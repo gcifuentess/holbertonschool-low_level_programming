6c290eeb9ed45f1920cbc85cfd761acd4516f228
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-13 23:19:19 -0500

    File updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct dog - data structure for a dog.
@@ -27,6 +26,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 
 #endif/*DOG_H*/
 
