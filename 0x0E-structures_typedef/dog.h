73244f7d7b4bf696142b41df546409b45c1f9759
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-13 19:48:31 -0500

    File created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data structure for a dog.
+ * @name: name of the dog.
+ * @owner: name of the owner of the dog.
+ * @age: age of the dog.
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif/*DOG_H*/
+
