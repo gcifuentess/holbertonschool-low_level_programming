36f9c93cfbb5a964f4c1ba2936e66beb58550fa7
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-13 23:12:12 -0500

    Copy of name and owner was missing. Free() of them was added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_t new;
 	dog_t *ptr;
+	char *name_copy, *owner_copy;
+
+	name_copy = strdup(name);
+	owner_copy = strdup(owner);
+	if (!name_copy || !owner_copy)
+	{
+		free(name_copy);
+		free(owner_copy);
+		return (NULL);
+	}
 
 	new.name = name;
 	new.age = age;
@@ -20,5 +30,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ptr = &new;
 
+	if (!ptr)
+	{
+		free(name_copy);
+		free(owner_copy);
+		return (NULL);
+	}
+
 	return (ptr);
+
 }
