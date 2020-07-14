c1bec04dd1fb0829b075d2a4a7feb27213975428
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-13 23:20:36 -0500

    strdup() forbidden. Now using _strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr;
 	char *name_copy, *owner_copy;
 
-	name_copy = strdup(name);
-	owner_copy = strdup(owner);
+	name_copy = _strdup(name);
+	owner_copy = _strdup(owner);
 	if (!name_copy || !owner_copy)
 	{
 		free(name_copy);
@@ -38,5 +38,48 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	return (ptr);
-
 }
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *    which contains a copy of the string given as a parameter.
+ * @str: the string to be copied.
+ *
+ * Description: -Returns a pointer to a new string which is a duplicate
+ *               of the string str. Memory for the new string is obtained
+ *               with malloc, and can be freed with free.
+ *              -Returns NULL if str = NULL.
+ *
+ * Return: On success, the _strdup function returns a pointer to the
+ *         duplicated string. It returns NULL if insufficient memory
+ *         was available
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	i++;
+
+	ptr = malloc(sizeof(char) * i);
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		ptr[j] = str[j];
+	}
+
+	return (ptr);
+}
+
