4dd7121f691a227c00540925598834cbadd9b489
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-15 15:55:15 -0500

    File created

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name in different ways
+ * @name: pointer of the name to print
+ * @f: pointer to the function that prints the name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
