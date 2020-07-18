ccccfb74dd269c231d3fc74585e8126adafe9882
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-18 17:29:52 -0500

    Added system call for exit

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -8,12 +8,16 @@
 		global	main
 
 		section	.text
-main:				; This is called by the C library startup code
+main:					; This is called by the C library startup code
 		mov     rax, 1		; system call for write
 		mov     rdi, 1		; file handle 1 is stdout
 		mov  	rsi, message	; address of string to output
 		mov 	rdx, 17		; number of bytes
-		syscall		; invoke operating system to do the write
+		syscall			; invoke operating system to do the write
+
+        	mov	rax, 0x02000001 ; system call for exit
+		xor	rdi, rdi	; exit code 0
+		syscall			; invoke operating system to exit
 
 		ret
 
