d28cf7586fc28f43c08a76b29bddea19f1f7aa0e
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-18 13:34:01 -0500

    Manage when n == 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
