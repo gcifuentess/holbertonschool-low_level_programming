07ebdf5d50ea921302e63756142c02079578d806
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-18 14:05:58 -0500

    Important change: the va_arg() return now is saved on a variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,6 +10,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
+	char *act_valist;
 	unsigned int i;
 
 	if (!n)
@@ -22,12 +23,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < n - 1 && separator)
-			printf("%s%s", va_arg(valist, char *), separator);
-		else if (i < n - 1 && separator && !va_arg(valist, char *))
-			printf("%s%s", va_arg(valist, char *), "(nil)");
+		act_valist = va_arg(valist, char *);
+		if (separator)
+			printf("%s%s", act_valist, separator);
+		else if (!act_valist)
+		{
+			printf("(nil)");
+		}
 		else
-			printf("%s", va_arg(valist, char *));
+			printf("%s", act_valist);
 	}
 	printf("\n");
 	va_end(valist);
