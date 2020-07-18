7e93bbfbd78b9c8b17dbe65294174ef6c3589d3f
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-18 16:38:58 -0500

    File created

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,17 @@
+;;  ----------------------------------------------------------------------------------------
+;;  Writes "Hello, Holberton" to the console using only system calls. Runs on 64-bit Linux only.
+;;  To assemble and run:
+;;
+;;      nasm -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o -o hello
+;;  ----------------------------------------------------------------------------------
+
+		global	main
+		extern	puts
+
+		section	.text
+main:						; This is called by the C library startup code
+		mov	rdi, message		; First integer (or pointer) argument in rdi
+		call	puts			; puts(message)
+		ret				; Return from main back into C library wrapper
+message:
+		db	"Hello, Holberton", 0 	; Note strings must be terminated with 0 in C
