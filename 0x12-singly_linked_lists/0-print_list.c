16ad13c2fc59795c26b6c839e0a5bf57a1c25394
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-23 21:56:36 -0500

    added: 0-print_list.c and 0-main.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to he list.
+ *
+ * Information: If str is NULL, print [0] (nil).
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+	list_t *toPrint;
+
+	n = 0;
+	toPrint = NULL;
+
+	if(!h)
+		return 0;
+
+	if ((h->str) != NULL)
+		printf("[%d] %s\n", h->len, h->str);
+	else
+		printf("[%d] %s\n", 0, "(nil)");
+	n++;
+	for (toPrint = h->next; toPrint; toPrint = toPrint->next)
+	{
+		if ((toPrint->str) != NULL)
+			printf("[%d] %s\n", toPrint->len, toPrint->str);
+		else
+			printf("[%d] %s\n", 0, "(nil)");
+		n++;
+	}
+
+	return (n);
+}
