156b495d61db957ab55a4733e07723ee97adf75b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-24 01:19:08 -0500

    added: 100-first.c 100-main.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void __attribute__((constructor)) calledFirst();
+
+void calledFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
