f6fc6c5a33e0804521b12c9b701a8376589ed733
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-23 22:07:57 -0500

    added: 1-list_len.c and 1-main.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: linked list.
+ *
+ * Return: number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	list_t *ptr;
+
+	if (h)
+		count = 1;
+	else
+		return (count = 0);
+
+	for (ptr = h->next; ptr->next; ptr = ptr->next)
+		count++;
+	count++;
+
+	return (count);
+}
