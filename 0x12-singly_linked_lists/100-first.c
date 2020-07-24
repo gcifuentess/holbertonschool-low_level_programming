147bd7f1f7041bdab0145129e7cf905d4c396d56
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-24 01:23:19 -0500

    fixed: 100-first.c - function documentation was missing

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 void __attribute__((constructor)) calledFirst();
 
+/**
+ * calledFirst - function that is called before main
+ *
+ * Return: Void
+ */
 void calledFirst(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
