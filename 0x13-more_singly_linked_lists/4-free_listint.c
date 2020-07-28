5903ecf0c2d8a76676ba06c3e8839b1b04d9c182
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-27 19:47:01 -0500

    Fixed: 4-free_listint.c - Now frees from the last node to head

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,10 +8,20 @@
 void free_listint(listint_t *head)
 {
 	listint_t *ptr;
+	int count, i;
 
 	if (!head)
 		return;
-
+	count = 0;
 	for (ptr = head; ptr; ptr = ptr->next)
+		count++;
+
+	for (; count; count--)
+	{
+		ptr = head;
+		for (i = 0; i < count; i++)
+			ptr = ptr->next;
 		free(ptr);
+	}
+	free(head);
 }
