fa40ce513b979a3238ecdbefb60459905e288dbe
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-28 20:19:04 -0500

    Fixed: 9-insert_nodeint.c - whole structure changed. Previous file dismissed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+listint_t *add_nodeint_between(listint_t **head, unsigned int idx, int n);
+
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: Double pointer to the head of the list.
@@ -7,15 +9,17 @@
  *       Index starts at 0.
  * @n: int to include in the new node.
  *
- * Important: If it is not possible to add the new node at index idx,
- *            do not add the new node and return NULL
+ * Important: -If it is not possible to add the new node at index idx,
+ *             do not add the new node and return NULL
+ *            -This function call other functions:
+ *             i)  add_nodeint_between()
+ *             ii) listint_len()
  *
  * Return: the address of the new node, or NULL if it failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int n_nodes;
-	listint_t *ptr, *ptrTemp;
 
 	if (!head)
 		return (NULL);
@@ -23,33 +27,37 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!(*head) && idx > 0)
 		return (NULL);
 
-	n_nodes = 0;
-	for (ptr = *head; ptr; ptr = ptr->next)
-		n_nodes++;
+	n_nodes = (unsigned int)listint_len(*head);
 
 	if (!(idx <= n_nodes))
 		return (NULL);
 
-	ptr = malloc(sizeof(listint_t));
-	if (!ptr)
-		return (NULL);
-
-	ptr->n = n;
-	if (head)
-	{
-		/*next will find the previous node before idx*/
-		ptrTemp = get_nodeint_at_index(*head, (idx - 1));
-		ptr->next = ptrTemp->next;
-		ptrTemp->next = ptr;
-	}
-	else
-	{
-		*head = ptr;
-		(*head)->next = NULL;
-	}
-
-	return (ptr);
+	return (add_nodeint_between(head, idx, n));
 }
+
+/**
+ * listint_len - returns the number of elements in a linked list.
+ * @h: linked list.
+ *
+ * Return: number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	listint_t *ptr, initial_node;
+	size_t count;
+
+	if (!h)
+		return (0);
+
+	initial_node = *h;
+	count = 0;
+
+	for (ptr = &initial_node; ptr; ptr = ptr->next)
+		count++;
+
+	return (count);
+}
+
 /**
  * get_nodeint_at_index - returns the nth node of a linked list.
  * @head: head of the linked list.
@@ -77,5 +85,55 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	ptr = head;
 	for (count = 0; count < index; count++)
 		ptr = ptr->next;
+	return (ptr);
+}
+
+/**
+ * add_nodeint_between - adds a new node in between of a linkd list.
+ * @head: head of the linked list.
+ * @idx: Is the index of the list where the new node should be added.
+ *       Index starts at 0.
+ * @n: int to add to the new node.
+ *
+ * Important: -Needs to be called by insert_nodeint_at_index() to
+ *             work properly.
+ *            -This function call other functions:
+ *             i)  malloc()
+ *             ii) get_nodeint_at_index()
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+listint_t *add_nodeint_between(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, *ptrTemp;
+
+	ptr = malloc(sizeof(listint_t));
+	if (!ptr)
+		return (NULL);
+
+	ptr->n = n;
+	if (*head && idx > 0)
+	{
+		/*next will find the previous node before idx*/
+		ptrTemp = get_nodeint_at_index(*head, (idx - 1));
+		ptr->next = ptrTemp->next;
+		ptrTemp->next = ptr;
+	}
+	else if (*head && idx == 0)
+	{
+		ptr->next = *head;
+		*head = ptr;
+	}
+	else if (!(*head) && idx == 0)
+	{
+		*head = ptr;
+		(*head)->next = NULL;
+	}
+	else
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	return (ptr);
 }
