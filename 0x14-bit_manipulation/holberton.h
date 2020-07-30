0bcc0b4668711938300f36a94fa1e5f3c681a8c3
Gabriel Cifuentes <1795@holbertonschool.com>
2020-07-30 16:58:11 -0500

    File added: holberton.h

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+/* Functions defined in this header: */
+int _strlen(const char *s);
+int _pow_recursion(int x, int y);
+
+#endif /* HOLBERTON_H */
