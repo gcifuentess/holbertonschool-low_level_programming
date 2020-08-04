26fd0877ec67b7a762d00169e67e2a69934afbc4
Gabriel Cifuentes <1795@holbertonschool.com>
2020-08-04 18:18:56 -0500

    File added: 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: the file to read.
+ * @letters: is the number of letters it should read and print.
+ *
+ * Return: -the actual number of letters it could read and print
+ *         -if the file can not be opened or read, return 0
+ *         -if filename is NULL return 0.
+ *         -if write fails or does not write the expected amount
+ *          of bytes, return 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, toPrint, printed;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = calloc(letters, sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	toPrint = read(fd, buf, letters);
+	if (toPrint == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	close(fd);
+
+	printed = write(1, buf, toPrint);
+	if (printed == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+
+	if (printed != toPrint)
+		return (0);
+
+	return (printed);
+}
