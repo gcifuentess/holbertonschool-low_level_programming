2ec60ba63275ffb720939c9ed5878b24384ff415
Gabriel Cifuentes <1795@holbertonschool.com>
2020-08-04 21:42:04 -0500

    Fixed: 1-create_file.c - fixed when text_content==NULL write() should be 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		check = write(fd, "", 1);
+		check = write(fd, "", 0);
 	else
 		check = write(fd, text_content, _strlen(text_content));
 
