dc20342fbf214a1182dd764a00c2b6c134e61e37
Gabriel Cifuentes <1795@holbertonschool.com>
2020-08-05 01:32:21 -0500

    Fixed: 3-cp.c - Back to one function -> variable names and if statemets shorted

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,60 +18,38 @@ int write_buffer(const char *argv1, const char *argv2);
 
 int main(int argc, char **argv)
 {
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, USAGE);
-		exit(97);
-	}
-
-	write_buffer(argv[1], argv[2]);
-
-	return (0);
-}
-
-/**
- * main - copies the content of a file to another through a buffer
- * @argv1: File Origin.
- * @argv2: File Destination.
- *
- * Return: 1 on success, -1 on failure.
- */
-int write_buffer(const char *argv1, const char *argv2)
-{
 	ssize_t check_to, check_from;
 	int fd_to, fd_from;
 	char *buffer[1024];
 	size_t buffer_size = 1024;
 
-	fd_from = open(argv1, O_RDONLY);
+	if (argc != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
+	fd_from = open(argv[1], O_RDONLY);
+
 	if (fd_from == -1)
-	{
-		dprintf(STDOUT_FILENO, NOREAD_ERR, argv1);
-		exit(98);
-	}
-	fd_to = open(argv2, O_CREAT | O_WRONLY | O_TRUNC, PERMISSIONS);
+		dprintf(STDOUT_FILENO, NOREAD_ERR, argv[1]), exit(98);
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, PERMISSIONS);
+
+	if (fd_to == -1)
+		dprintf(STDOUT_FILENO, NOWRITE_ERR, argv[2]), exit(99);
+
 	while ((check_from = read(fd_from, buffer, buffer_size)) > 0)
-	{
 		if (write(fd_to, buffer, check_from) != check_from)
-		{
-			dprintf(STDOUT_FILENO, NOWRITE_ERR, argv2);
-			exit(99);
-		}
-	}
+			dprintf(STDOUT_FILENO, NOWRITE_ERR, argv[2]), exit(99);
+
 	if (check_from == -1)
-	{
-		dprintf(STDOUT_FILENO, NOREAD_ERR, argv1);
-		exit(98);
-	}
+		dprintf(STDOUT_FILENO, NOREAD_ERR, argv[1]), exit(98);
+
 	check_from = close(fd_from);
 	check_to = close(fd_to);
+
 	if (check_from == -1)
-		dprintf(STDERR_FILENO, NOCLOSE_ERR, fd_from);
+		dprintf(STDERR_FILENO, NOCLOSE_ERR, fd_from), exit(100);
 	if (check_to == -1)
-		dprintf(STDERR_FILENO, NOCLOSE_ERR, fd_to);
-	if (check_to == -1 || check_from == -1)
-		exit(100);
+		dprintf(STDERR_FILENO, NOCLOSE_ERR, fd_to), exit(100);
 
 	return (0);
 }
