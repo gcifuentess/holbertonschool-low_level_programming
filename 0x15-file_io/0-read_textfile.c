13d3fe2e763a45b6b680d6df995a32e0ad7a56af
Gabriel Cifuentes <1795@holbertonschool.com>
2020-08-05 01:35:28 -0500

    Fixed: 0-read_textfile.c - Betty Style fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: the file to read.
  * @letters: is the number of letters it should read and print.
  *
