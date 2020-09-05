5bbdb1be012eb5d784cfef39cc8c8a90d6918d35
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-05 23:20:44 +0000

    File fixed: 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 
+dlistint_t *get_dnodeint_at_index_2(dlistint_t *head, unsigned int index);
+size_t dlistint_len_2(const dlistint_t *h);
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: head of the list
@@ -16,10 +19,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 
-	node = get_dnodeint_at_index(*h, idx);
+	node = get_dnodeint_at_index_2(*h, idx);
 	if (!node || idx == 0)
 	{
-		n_nodes = dlistint_len(*h);
+		n_nodes = dlistint_len_2(*h);
 		if (idx == 0)
 			new = add_dnodeint(h, n);
 		else if (idx == n_nodes)
@@ -39,4 +42,41 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node->prev = new;
 	}
 	return (new);
+}
+/**
+ * get_dnodeint_at_index_2 - returns the nth node of a dlistint_t linked list.
+ * @head: head of the list.
+ * @index: is the index of the node, starting from 0.
+ *
+ * Return: returns the nth node, NULL if it doesn't exist.
+ */
+dlistint_t *get_dnodeint_at_index_2(dlistint_t *head, unsigned int index)
+{
+	size_t n_nodes, i;
+	dlistint_t *node;
+
+	n_nodes = dlistint_len_2(head);
+	if ((index + 1) > n_nodes || !n_nodes)
+		return (NULL);
+	for (i = 0, node = head; i < index; i++)
+		node = node->next;
+	return (node);
+}
+
+/**
+ * dlistint_len_2 - eturns the number of elements in a linked dlistint_t list
+ * @h: header of the list
+ *
+ * Return: the number of elements in a linked dlistint_t list.
+ */
+size_t dlistint_len_2(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current;
+
+	if (!h)
+		return (count);
+	for (current = h; current; current = current->next)
+		count++;
+	return (count);
 }
