e7bc41f602f9584cf60f053a6cf39d4ef29b0da8
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-07 19:31:11 +0000

    File added: README.md; holberton.h; libholberton.so; object_files.sh

---
+++ 0x18-dynamic_libraries/object_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+gcc -fPIC -c 0-isupper.c
+gcc -fPIC -c 0-main.c
+gcc -fPIC -c 0-memset.c
+gcc -fPIC -c 0-strcat.c
+gcc -fPIC -c 100-atoi.c
+gcc -fPIC -c 1-isdigit.c
+gcc -fPIC -c 1-memcpy.c
+gcc -fPIC -c 1-strncat.c
+gcc -fPIC -c 2-strchr.c
+gcc -fPIC -c 2-strlen.c
+gcc -fPIC -c 2-strncpy.c
+gcc -fPIC -c 3-islower.c
+gcc -fPIC -c 3-puts.c
+gcc -fPIC -c 3-strcmp.c
+gcc -fPIC -c 3-strspn.c
+gcc -fPIC -c 4-isalpha.c
+gcc -fPIC -c 4-strpbrk.c
+gcc -fPIC -c 5-strstr.c
+gcc -fPIC -c 6-abs.c
+gcc -fPIC -c 9-strcpy.c
+gcc -fPIC -c _putchar.c
+
