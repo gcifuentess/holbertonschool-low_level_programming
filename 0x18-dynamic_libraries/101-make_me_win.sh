b84afa309627e9e18b3d9d65fb59c9cacb35a54b
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-08 07:40:15 +0000

    File added: 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/gcifuentess/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libMakeMeWin.so
-export LD_PRELOAD="/tmp/libMakeMeWin.so"
-
+export LD_PRELOAD=/tmp/libMakeMeWin.so
