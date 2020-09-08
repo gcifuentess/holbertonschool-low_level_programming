7177297381215354ba6b91f23581f4f43e14c61c
Gabriel Cifuentes <1795@holbertonschool.com>
2020-09-08 00:03:20 +0000

    Files added: 100-operations.so; 100-tests.py; c_basic_calc_func.c

---
+++ 0x18-dynamic_libraries/c_basic_calc_func.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - sums two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtrac two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the subtraction of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the division of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the module between two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the module between a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
