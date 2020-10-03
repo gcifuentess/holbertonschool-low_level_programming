f8e521339d6dd202893e6aad308c258311ba7dde
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-03 04:05:26 +0000

    File fixed: 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,16 +11,23 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new;
 	char *value_cpy, *key_cpy;
+	int colission;
 
+	colission = 0;
 	if (!key)
 		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
-	value_cpy = strdup(value);
 	key_cpy = strdup(key);
-	if (!value_cpy | !key_cpy)
+	if (!key_cpy)
 		return (0);
+	value_cpy = strdup(value);
+	if (!value_cpy)
+	{
+		free(key_cpy);
+		return (0);
+	}
 	if (!ht->array[index])
 	{
 		ht->array[index] = malloc(sizeof(hash_node_t));
@@ -35,6 +42,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		(ht->array[index])->next = NULL;
 		return (1);
 	}
+
+	colission = add_node(ht, key_cpy, value_cpy, index);
+	return (colission);
+}
+
+/**
+ * add_node - adds a node to the hash table
+ * @ht: pointer to the hash table
+ * @key_cpy: string copy of the key
+ * @value_cpy: string copy of the value
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int add_node(hash_table_t *ht, char *key_cpy, char *value_cpy,
+	     unsigned long int index)
+{
+	hash_node_t *new;
+
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 	{
@@ -46,5 +71,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = key_cpy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
+
 	return (1);
 }
