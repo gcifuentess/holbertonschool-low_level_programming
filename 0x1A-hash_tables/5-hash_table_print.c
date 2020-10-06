435dfb5065bad63ae679418bea5342ad47385ac9
Gabriel Cifuentes <1795@holbertonschool.com>
2020-10-06 03:57:15 +0000

    Files added: 5-hash_table_print.c 5-main.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: Void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *head;
+	int flag;
+
+	if (!ht)
+		printf("{}\n");
+
+	printf("{");
+	flag = 0;
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		head = ht->array[idx];
+		if (!head)
+			continue;
+		while (head)
+		{
+			if (flag == 1)
+				printf(", ");
+			else
+				flag = 1;
+			printf("'%s': '%s'", head->key, head->value);
+			head = head->next;
+		}
+	}
+	printf("}\n");
+}
